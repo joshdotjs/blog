17f4dc92ae103eed6dac9998c557c602ed89dccf
joshdotjs <joshdotjs@gmail.com>
2023-07-11 05:34:15 -0500

    001-tone

--- web-audio/001-tone/blog-001-tone.js
+++ web-audio/001-tone/blog-001-tone.js
@@ -1,11 +1,10 @@
 // create a synth and connect it to the master output (your speakers)
-let synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination();
 
 function playSound() {
   // trigger a C4 note immediately
   synth.triggerAttackRelease("C4", "8n");
-
-  console.log('clicked');
 }
 
-document.getElementById('play-button').addEventListener('click', playSound);
+const play_btn = document.getElementById('play-button');
+play_btn?.addEventListener('click', playSound);
