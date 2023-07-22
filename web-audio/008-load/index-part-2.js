5f91e34411913624e7ad33a4aa22075ff913446b
joshdotjs <joshdotjs@gmail.com>
2023-07-22 05:48:45 -0500

    play uploaded file

--- web-audio/008-load/index-part-2.js
+++ web-audio/008-load/index-part-2.js
@@ -1,19 +1,23 @@
 // ==============================================
 
+function loadHandler (event) {
+  const file = event.target.files[0];
+  console.log('file: ', file);
+
+  const url = URL.createObjectURL(file);
+  document.getElementById('track').setAttribute('src', url);
+}
+
+// ==============================================
+
+
 const setupFileUpload = () => {
   const input = document.querySelector("#input");
   if (!input) throw new Error("No input element found");
-
-  function handler (event) {
-    console.log('event: ', event);
-    console.log('event.target: ', event?.target);
-    console.log('event.target.files: ', event?.target.files);
-
-    const file_list = this.files;
-    console.log('file_list: ', file_list);
-  }
-  input.addEventListener("change", handler, false);
+  input.addEventListener("change", loadHandler, false);
 };
+
+// ==============================================
 
 // ==============================================
 
