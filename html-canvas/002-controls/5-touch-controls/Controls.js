f62d1f03782946beee494a844e91cb1935b45799
joshdotjs <joshdotjs@gmail.com>
2023-08-08 05:05:18 -0500

    record touch events

---
+++ html-canvas/002-controls/5-touch-controls/Controls.js
@@ -0,0 +1,56 @@
+// touch start
+// touch move
+// touch end
+
+// event object:
+// -touches:       where screen is touched
+// -targetTouches: where you are touching within the element
+// -changeTouches: all of the touches that have changes since the last time the event fired
+
+document.addEventListener("touchstart", e => {
+  console.log('start');
+
+  [...e.changedTouches].forEach((touch) => {
+    const div = document.createElement('div');
+    div.classList.add('touch');
+    div.style.top = `${touch.pageY}px`;
+    div.style.left = `${touch.pageX}px`;
+    div.id = touch.identifier; // index in array for each touch 
+    document.body.appendChild(div);
+  });
+});
+document.addEventListener("touchmove",  e => {
+  console.log('move');
+});
+document.addEventListener("touchend",   e => {
+  console.log('end');
+});
+
+
+
+class Controls {
+  constructor() {
+    this.keys = {};
+
+    document.addEventListener("keydown", e => {
+      if (['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', ' '].indexOf(e.key) >= 0) e.preventDefault();
+      this.keys[e.key] = true;
+    }, false);
+
+    document.addEventListener("keyup", e => this.keys[e.key] = false, false);
+  }
+
+  get x() {
+    if (this.keys['ArrowLeft']  || this.keys['a'])  return -1;
+    if (this.keys['ArrowRight'] || this.keys['d'])  return 1;
+    return 0;
+  }
+
+  get y() {
+    if (this.keys['ArrowUp']   || this.keys['w'])  return -1;
+    if (this.keys['ArrowDown'] || this.keys['s'])  return 1;
+    return 0;
+  }
+}
+
+export default Controls;
---
+++ html-canvas/002-controls/5-touch-controls/index.js
@@ -0,0 +1,39 @@
+import Controls from "./Controls.js";
+
+// ==========================================
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// ==========================================
+
+const controls = new Controls();
+
+// ==========================================
+
+// Game setup code
+let x = canvas.width  / 2 - 25;
+let y = canvas.height / 2 - 25;
+
+const update = () => {
+  x += controls.x * 3;
+  y += controls.y * 3;
+};
+
+const render = () => {
+  ctx.fillStyle = 'rgba(0, 0, 0)';
+  ctx.fillRect(x, y, 50, 50);
+};
+
+// ==========================================
+
+function animate(t1) {
+  requestAnimationFrame(animate);
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  update();
+  render();
+
+}
+animate();
