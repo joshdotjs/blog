55ccadee5a5e00a5fca7d7df31f80d15fae3ac8e
joshdotjs <joshdotjs@gmail.com>
2023-08-09 04:22:00 -0500

    touch controlls functional

--- html-canvas/002-controls/6-touch-controls-functional/Controls.js
+++ html-canvas/002-controls/6-touch-controls-functional/Controls.js
@@ -4,14 +4,37 @@ let keys = {};
 
 // ==============================================
 
+const touch_controller_up = document.querySelector('#touch-controller-up');
+const touch_controller_down = document.querySelector('#touch-controller-down');
+const touch_controller_left = document.querySelector('#touch-controller-left');
+const touch_controller_right = document.querySelector('#touch-controller-right');
+
+// ==============================================
+
+// Keyboard Controls:
+
 document.addEventListener("keydown", (e) => {
   if (['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', ' '].indexOf(e.key) >= 0) e.preventDefault();
   keys[e.key] = true;
 }, false);
 
+document.addEventListener("keyup", ({ key }) => keys[key] = false, false);
+
 // ==============================================
 
-document.addEventListener("keyup", ({ key }) => keys[key] = false, false);
+// Touch Controls:
+
+touch_controller_up.addEventListener('touchstart',    () => keys['ArrowUp']    = true);
+touch_controller_up.addEventListener('touchend',      () => keys['ArrowUp']    = false);
+
+touch_controller_down.addEventListener('touchstart',  () => keys['ArrowDown']  = true);
+touch_controller_down.addEventListener('touchend',    () => keys['ArrowDown']  = false);
+
+touch_controller_left.addEventListener('touchstart',  () => keys['ArrowLeft']  = true);
+touch_controller_left.addEventListener('touchend',    () => keys['ArrowLeft']  = false);
+
+touch_controller_right.addEventListener('touchstart', () => keys['ArrowRight'] = true);
+touch_controller_right.addEventListener('touchend',   () => keys['ArrowRight'] = false);
 
 // ==============================================
 
@@ -28,10 +51,7 @@ const y = () => {
 
 // ==============================================
 
-const controls = {
-  x,
-  y
-};
+const controls = { x, y };
 
 // ==============================================
 
