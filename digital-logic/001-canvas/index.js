3d4fbaa2350832e38ff940c21092ca3714ac5a44
joshdotjs <joshdotjs@gmail.com>
2023-08-17 06:45:05 -0500

    select and place element logic

--- digital-logic/001-canvas/index.js
+++ digital-logic/001-canvas/index.js
@@ -3,7 +3,8 @@ import { canvas, ctx } from "./util.js";
 // ==========================================
 
 const createElement = (type) => document.createElement(type);
-const qs = (str, elem=document) => elem.querySelector(str);
+const qs  = (str, elem=document) => elem.querySelector(str);
+const qsa = (str, elem=document) => elem.querySelectorAll(str);
 
 // ==========================================
 
@@ -24,28 +25,80 @@ const qs = (str, elem=document) => elem.querySelector(str);
 
 // ==========================================
 
+// const loadImg = (str) => {
+//   const image = new Image();
+//   image.src = `./${str}`;
+//   return image; 
+// };
+
+// const img = loadImg('lena.png');
+// ctx.drawImage(img, 0, 0, 50, 50);
+
+// const image = new Image(60, 45); // Using optional size for image
+// image.onload = function() {
+
+//   // Load an image of intrinsic size 300x227 in CSS pixels
+//   image.src = "/digital-logic/001-canvas/lena.png";
+//   ctx.drawImage(image, 0, 0);
+// }; // Draw when image has loaded
+
+// ==========================================
+
+
+let selected_element = {
+  type: null,
+};
+
+// ==========================================
+
+const setupElementPicker = () => {
+
+  const dom_elements = qsa('section#elements-picker > .element');
+  console.log('dom_elements: ', dom_elements);
+
+
+  dom_elements.forEach((dom_element) => {
+    
+    const element_type = dom_element.dataset.type;
+
+    dom_element.addEventListener('click', () => {
+      if (element_type === 'and') {
+        selected_element.type = 'and';
+      } else
+      if (element_type === 'not') {
+        selected_element.type = 'not';
+      }
+
+      console.log('selected element: ', selected_element);
+    });
+  });
+};
+setupElementPicker();
+
+// ==========================================
+
 const setupPoints = () => {
   
-  const main = qs('main');
+  const canvas_container = qs('section#canvas');
   
-  const GAP = 50;
-  const WIDTH = 50;
+  const GAP = 40;
+  const WIDTH = 20;
   
-  for (let i = 0; i < 3; ++i) {
-    for (let j = 0; j < 3; ++j) {
+  for (let i = 0; i < 5; ++i) {
+    for (let j = 0; j < 5; ++j) {
       const elem = createElement('div');
       elem.classList.add('point');
       elem.style.top = `${i * (WIDTH + GAP)}px`;
       elem.style.left = `${j * (WIDTH + GAP)}px`;
-      main.appendChild(elem);
+      canvas_container.appendChild(elem);
 
       elem.addEventListener('click', () => {
-        console.log('i: ', i, '\tj: ', j);
+        if (selected_element.type !== null) {
+          console.log('place element: ', selected_element, '\ni: ', i, '\tj: ', j);
+          selected_element.type = null;
+        }
       });
     }
   }
-
-
-
 };
 setupPoints();
