9f362fa85392e28295aed6475310554587aeac59
joshdotjs <joshdotjs@gmail.com>
2023-08-18 17:54:50 -0500

    logic

--- quiz/index.js
+++ quiz/index.js
@@ -101,26 +101,32 @@ const already_asked_question_indices = [];
 //  -1.a: question is displayed
 //  -1.b: options are displayed
 
-// 2. all answers are populated 
-// 3. when user clicks option we check if it is correct
+// 2. when user clicks option we check if it is correct
 // 4. if correct we display correct message and increase score
-// 5. if incorrect we display incorrect message and decrease score
-// 6. we display next question
-// 7. we repeat steps 1-6 until all questions are answered
-// 8. we display final score
+// 5. we display next question
+// 6. we repeat steps 1-6 until all questions are answered
+// 7. we display final score
 
 
 const DOM_quesion = document.querySelector('section#question');
 const DOM_optioms = document.querySelectorAll('section#options  >  .option');
+const DOM_score = document.querySelector('section#score  >  #score-value  >  span');
+const DOM_num_remaining_quesions = document.querySelector('section#score  >  #num-remaining-quesions  >  span');
 console.log('DOM_optioms', DOM_optioms);
+
+let score = 0;
 
 const runQuestion = () => {
 
+  
   // 0.a quesion is randomly selected and options are shuffled
   const { element: question, randomly_chosen_element_index: randomly_chosen_question_index } = randomlyChooseElementExcludingIndices(questions, already_asked_question_indices);
 
   // 0.b: add the index of the question to the already_asked_question_indices array
   already_asked_question_indices.push(randomly_chosen_question_index);
+  console.log('already_asked_question_indices: ', already_asked_question_indices);
+  const num_remaining_quesions = questions.length - already_asked_question_indices.length;
+  DOM_num_remaining_quesions.textContent = num_remaining_quesions;
   
   // 0.c: options are shuffled
   const { shuffled: shuffled_options, correct_index } = shuffle(question.options);
@@ -132,7 +138,37 @@ const runQuestion = () => {
   //  -1.b: options are displayed
   DOM_optioms.forEach((DOM_option, idx) => {
     DOM_option.textContent = shuffled_options[idx];
+
+
+    // 2. when user clicks option we check if it is correct
+    DOM_option.onclick = () => {
+      // 4. if correct we display correct message and increase score
+      if (idx === correct_index) {
+        DOM_option.classList.add('correct');
+        score++;
+      } else {
+        // 5. if incorrect we display incorrect message and decrease score
+        DOM_option.classList.add('incorrect');
+      }
+
+      // update score
+      DOM_score.textContent = score;
+
+      // 6. we repeat steps 1-5 until all questions are answered
+      if (num_remaining_quesions > 0) {
+        // 5. we display next question
+        runQuestion();
+      } else {
+        // 7. we display final score
+        alert(`Game over!  Score: ${score}`);
+      }
+
+    };
   });
+
+  
+
+
 };
 
 runQuestion();
